7bac23f088bf9d5d31b4286a738f12dca53980d8
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-04-29 17:35:11 +0300

    05_hash_tables_ES6_02_check_voter

--- JS/grokking_algorithms_book/05_hash_tables/ES6/01_price_of_groceries.js
+++ JS/grokking_algorithms_book/05_hash_tables/ES6/01_price_of_groceries.js
@@ -1,19 +1,8 @@
-const voted = {};
+const book = {};
+// an apple costs 67 cents
+book.apple = 0.67;
+// milk costs $1.49
+book.milk = 1.49;
+book.avocado = 1.49;
 
-/**
- * Vote check
- * @param {string} name Voter name
- */
-
-const checkVoter = name => {
-    if (voted[name]) {
-        console.log('kick them out!');
-    }else {
-        voted[name] = true;
-        console.log("let them vote!")
-    }
-};
-
-checkVoter("tom"); // let them vote!
-checkVoter("mike"); // let them vote!
-checkVoter("mike"); // let them vote!
+console.log(book); // { apple: 0.67, milk: 1.49, avocado: 1.49 }
